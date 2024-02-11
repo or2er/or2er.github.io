7e64bed769f0f09d52cf5e48855c0dc2645df398
Supporter09 <maivannhatminh2005@gmail.com>
2024-02-11 10:28:10 +0700

    feat: Adding navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-      <h1 className="text-4xl font-bold font-right_grotesk">
-        What is our identity and how may we help you?
-      </h1>
-    </main>
+    <div>
+      <Navbar />
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24`}
+      ></main>
+    </div>
   );
 }
