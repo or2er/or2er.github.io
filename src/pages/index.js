8c6163741d87217d6f8fecd7c8e99d537bb70b49
Supporter09 <maivannhatminh2005@gmail.com>
2024-02-11 22:41:50 +0700

    style: adding responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,39 +29,39 @@ export default function Home() {
     <div>
       <Navbar />
       <main
-        className={`min-h-screen flex flex-col justify-center p-24 overflow-hidden`}
+        className={`min-h-screen flex flex-col justify-center py-24 px-6 lg:px-24 overflow-hidden`}
       >
         <div className="text-center">
-          <h1 className="text-[600px] text-[#F10000] leading-none font-right_grotesk">
+          <h1 className="text-[250px] md:text-[300px] xl:text-[500px] text-[#F10000] leading-none font-right_grotesk">
             BEYOND THE
           </h1>
-          <h1 className="text-[600px] leading-[0.6] font-right_grotesk text-stroke">
+          <h1 className="text-[250px] md:text-[300px] xl:text-[500px] leading-[0.6] font-right_grotesk text-stroke">
             EXPECTATION
           </h1>
         </div>
 
         <div className="py-16 relative">
-          <h1 className="text-[120px] font-bold">
+          <h1 className="text-[60px] md:text-[80px] xl:text-[120px] font-bold">
             What is our identity
             <br /> and how <br />
             may we help you?
           </h1>
-          <div className="h-24 w-auto aspect-[7/1] bg-[#F10000] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" />
+          <div className="h-16 xl:h-24 w-auto aspect-[3.5/1] 2xl:aspect-[7/1] bg-[#F10000] absolute top-[50%] left-[50%] translate-x-[-30%] lg:translate-x-[-50%] translate-y-[-52%] md:translate-y-[-60%]" />
           <p className="text-2xl">Team or2er</p>
           <Image
             src={decor}
             alt="decor"
-            className="absolute bottom-[-6rem] left-auto right-[-15rem]"
+            className="absolute bottom-[-6rem] left-auto right-[-15rem] w-[45%] h-auto lg:w-auto lg:h-auto"
           />
         </div>
 
         <div className="flex-row grid grid-cols-2 pt-56 ">
-          <p className="text-4xl pr-24 leading-normal">
+          <p className="text-2xl/normal xl:text-4xl/normal pr-16 xl:pr-24">
             We are five enthusiastic developers, aged 19-20, form the or2er
             team. All of whom are{" "}
-            <p className="text-[#F10000] underline underline-offset-8">
+            <span className="text-[#F10000] underline underline-offset-8">
               dedicated to using AI to create solutions
-            </p>{" "}
+            </span>{" "}
             that makes a positive impact. Each member brings their own skills
             and ideas, working together seamlessly to push the boundaries of AI.
           </p>
