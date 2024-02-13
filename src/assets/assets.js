436d34a30268e543a89afce1da6e1079a49c2927
Supporter09 <maivannhatminh2005@gmail.com>
2024-02-13 22:41:44 +0700

    feat: complete basic ui frame

---
+++ src/assets/assets.js
@@ -0,0 +1,16 @@
+// SVGs
+import decor from "./decor.svg";
+import image_decor from "./image_decor.svg";
+import logo from "./v2.1_black.svg";
+
+// Images
+import dmn_pnq from "./dmn&pnq.png";
+import p1 from "./p1.png";
+import p2 from "./p2.png";
+import p3 from "./p3.png";
+import p4 from "./p4.png";
+import p5 from "./p5.png";
+import p6 from "./p6.png";
+import p7 from "./p7.png";
+
+export { decor, image_decor, logo, dmn_pnq, p1, p2, p3, p4, p5, p6, p7 };
---
+++ src/constants/constants.js
@@ -0,0 +1,47 @@
+import {
+  decor,
+  image_decor,
+  logo,
+  dmn_pnq,
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
+} from "@/assets/assets";
+
+// Journey
+const journey = [
+  {
+    img: p1,
+    desc: "It all began in the vibrant atmosphere of a hot pot restaurant, where five like-minded individuals, each driven by a fervent passion for technology, serendipitously crossed paths. Instantly connecting over shared ambitions and a mutual love for innovation, we decided to join forces and form a new team - or2er.",
+  },
+  {
+    img: p2,
+    desc: "Over steaming bowls of hot pot, the newly formed team delves into animated discussions about their collective vision for the future. Brimming with excitement and fueled by our shared vision, we brainstormed ideas and set ambitious goals, eager to leave our mark on the tech industry.",
+  },
+  {
+    img: p3,
+    desc: "With our determination ablaze, we eagerly dove into our inaugural hackathon, the IAI Hackathon. Armed with creativity and camaraderie, we tackled challenges head-on, showcasing our technical prowess and collaborative spirit.",
+  },
+  {
+    img: p4,
+    desc: "Through countless hours of hard work and unwavering dedication, we proudly clinched the second prize in our first hackathon, a testament to our teamwork and relentless pursuit of excellence.",
+  },
+  {
+    img: p5,
+    desc: "Encouraged by our initial success, we set our sights higher, aspiring to participate in larger, more prestigious hackathons. We envisioned these challenges as opportunities to push our limits and further expand our horizons.",
+  },
+  {
+    img: p6,
+    desc: "Undaunted by the fierce competition and the impressive array of projects at the Samsung  Hackathon, we fearlessly embrace the challenge, pouring our hearts and souls into our project.",
+  },
+  {
+    img: p7,
+    desc: "Despite facing formidable opponents and ultimately securing the 4th place, we emerged from the experience with heads held high. We took pride in our journey, knowing that our hard work, resilience, and passion for technology had propelled us forward, laying the groundwork for even greater achievements ahead.",
+  },
+];
+
+export { journey };
