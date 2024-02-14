5e10a0a649d7c21a6f87a6c945fb5a713610e693
Supporter09 <maivannhatminh2005@gmail.com>
2024-02-14 11:08:15 +0700

    chore: config tabspace, prettier and add check and write prettier cmd in package.json

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,29 +6,29 @@ import People from "@/sections/people";
 import Journey from "@/sections/journey";
 
 function HightlightText({ text }) {
-  processed_text = text.split("");
-  console.log("processed text: ", processed_text);
+    processed_text = text.split("");
+    console.log("processed text: ", processed_text);
 
-  return (
-    <div className="block relative text-center">
-      {processed_text.map((char, index) => (
-        <div
-          key={index}
-          className="relative inline-block transform-none rotate-0 "
-        >
-          {char}
+    return (
+        <div className="block relative text-center">
+            {processed_text.map((char, index) => (
+                <div
+                    key={index}
+                    className="relative inline-block transform-none rotate-0 "
+                >
+                    {char}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <People />
-      <Journey />
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <People />
+            <Journey />
+        </div>
+    );
 }
