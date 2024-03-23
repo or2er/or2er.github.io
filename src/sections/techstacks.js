10e69c8485dbbe57ec6188ccbcc948750761e3f1
Supporter09 <maivannhatminh2005@gmail.com>
2024-03-23 20:18:52 +0700

    feat: add responsive for the techstack section

--- src/sections/techstacks.js
+++ src/sections/techstacks.js
@@ -14,7 +14,9 @@ import Image from "next/image";
 
 export default function TechStacks() {
   return (
-    <section className={`grid grid-cols-2 py-24 px-6 lg:px-24 overflow-hidden`}>
+    <section
+      className={`grid grid-cols-1 lg:grid-cols-2 py-24 px-6 lg:px-24 overflow-hidden`}
+    >
       <div className="flex flex-col">
         <div className="text-left">
           <h1 className="text-[180px] xl:text-[200px] text-[#000000] leading-none font-right_grotesk">
@@ -25,42 +27,79 @@ export default function TechStacks() {
           </h1>
         </div>
 
-        <p className="text-1xl md:text-3xl pb-16">- 1+ years of client work</p>
+        <p className="text-1xl md:text-3xl pb-16">
+          - 1+ years of client work
+        </p>
         <p className="text-1xl md:text-3xl pr-16">
-          Members of Team or2er come from a variety of professional backgrounds.
-          Our tech stack is made up of several cutting-edge framework and tool
-          components working together.
+          Members of Team or2er come from a variety of professional
+          backgrounds. Our tech stack is made up of several
+          cutting-edge framework and tool components working together.
         </p>
       </div>
 
-      <div className="">
-        <div className="flex flex-row justify-between items-center pb-6">
-          <Image src={scikit} alt="scikit" className="h-[100%] object-cover" />
-          <Image src={pytorch} alt="scikit" className="h-[100%] object-cover" />
+      <div className="grid grid-cols-3 gap-4 pt-12 lg:pt-0 ">
+        <div className="flex justify-center">
+          <Image
+            src={scikit}
+            alt="scikit"
+            className="h-[100%] object-contain"
+          />
+        </div>
+        <div className="flex justify-center">
+          <Image
+            src={pytorch}
+            alt="scikit"
+            className="h-[100%] object-contain"
+          />
+        </div>
+        <div className="flex justify-center">
           <Image
             src={tensorflow}
             alt="scikit"
-            className="h-[100%] object-cover"
+            className="h-[100%] object-contain"
           />
         </div>
-        <div className="flex flex-row justify-between items-center pb-6">
-          <Image src={nodejs} alt="scikit" className="h-[100%] object-cover" />
-          <Image src={nextjs} alt="scikit" className="h-[100%] object-cover" />
-          <Image src={mongodb} alt="scikit" className="h-[100%] object-cover" />
+        <div className="flex justify-center">
+          <Image
+            src={spring}
+            alt="scikit"
+            className="h-[100%] object-contain"
+          />
         </div>
-        <div className="flex flex-row justify-between items-center pb-6">
-          <Image src={spring} alt="scikit" className="h-[100%] object-cover" />
-          <Image src={neo4j} alt="scikit" className="h-[100%] object-cover" />
-          <Image src={redis} alt="scikit" className="h-[100%] object-cover" />
+        <div className="flex justify-center">
+          <Image
+            src={neo4j}
+            alt="scikit"
+            className="h-[100%] object-contain"
+          />
         </div>
-        <div className="flex flex-row justify-between items-center pb-6">
-          <Image src={mongodb} alt="scikit" className="h-[100%] object-cover" />
+        <div className="flex justify-center">
+          <Image
+            src={redis}
+            alt="scikit"
+            className="h-[100%] object-contain"
+          />
+        </div>
+        <div className="flex justify-center">
+          <Image
+            src={mongodb}
+            alt="scikit"
+            className="h-[100%] object-contain"
+          />
+        </div>
+        <div className="flex justify-center">
           <Image
             src={tensorflow}
             alt="scikit"
-            className="h-[100%] object-cover"
+            className="h-[100%] object-contain"
           />
-          <Image src={nodejs} alt="scikit" className="h-[100%] object-cover" />
+        </div>
+        <div className="flex justify-center">
+          <Image
+            src={nodejs}
+            alt="scikit"
+            className="h-[100%] object-contain"
+          />
         </div>
       </div>
     </section>
